efe9d70f5e295bfd13cef0acc615eea058d15c99
yoon <stay_on_track@naver.com>
2025-03-19 11:19:27 +0900

    환경 설정 파일 및 gitignore

---
+++ project/backend/src/main/java/godsang/backend/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package godsang.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Restful API")
+                .description("소통을 위한 API 명세서")
+                .version("1.0.0");
+    }
+}
