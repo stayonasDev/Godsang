3f46f516458bb5d3bc99762e0bd58f73f163da61
yoon <stay_on_track@naver.com>
2025-03-19 10:26:33 +0900

    a

---
+++ project/backend/src/main/java/godsang/backend/GodsangApplication.java
@@ -0,0 +1,13 @@
+package godsang.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodsangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodsangApplication.class, args);
+	}
+
+}
---
+++ project/backend/src/test/java/godsang/backend/GodsangApplicationTests.java
@@ -0,0 +1,13 @@
+package godsang.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodsangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
